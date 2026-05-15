e98db0a0925ac8009bea9c1b39a9b5de7f0b16f8
chinxeleer <blessingeleer.programming@gmail.com>
2026-05-15 18:59:32 +0200

    initial launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   fonts: [
     {
-      provider: fontProviders.local(),
-      name: "Elanor-ExBdlt",
-      cssVariable: "--font-Elanor",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/Elanor-bold.otf"],
-            weight: "bold",
-            style: "oblique",
-          },
-          
-        ],
-      },
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Inder",
-      cssVariable: "--font-Inder",
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-Inter",
+      weights: [400, 800],
     },
   ],
+
+  integrations: [react()],
 });
