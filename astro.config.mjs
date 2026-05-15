211201b5dc02a30b0f7dfcdae92977cf3c29e4ff
chinxeleer <blessingeleer.programming@gmail.com>
2026-05-15 02:10:18 +0200

    adds fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Elanor-ExBdlt",
+      cssVariable: "--font-Elanor",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Elanor-bold.otf"],
+            weight: "bold",
+            style: "oblique",
+          },
+          
+        ],
+      },
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inder",
+      cssVariable: "--font-Inder",
+    },
+  ],
+});
